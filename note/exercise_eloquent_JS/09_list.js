cdf3af4e8e5f8364109d46a418656f420baa3b43
interval-ch <60610169+interval-ch@users.noreply.github.com>
2022-08-08 10:40:35 +0800

    01

--- note/exercise_eloquent_JS/09_list.js
+++ note/exercise_eloquent_JS/09_list.js
@@ -1,31 +1,30 @@
-// const arrayToList = (arr) => {
-// 	//base
-// 	let list = {};
-// 	list.value = arr[arr.length-1];
-// 	list.rest = null;
-// 	console.log(list);
-// 	//others
-// 	for (let i = arr.length-2 ; i>=0; i--) {
-// 		let newList = {};
-// 		newList.rest = list;
-// 		newList.value = arr[i];
-// 		console.log(newList);
-// 		}
-// 	}
-// let arr = [1,2,3];
-// console.log(arrayToList(arr));
-
-function arrayToList(array) {
+const arrayToList = (array) => {
 	let list = null;
-	for (let i = array.length - 1; i >= 0; i--) {
-	  list = {value: array[i], rest: list};
+	for (let i = array.length-1; i >= 0 ; i--) {
+		list = {value: array[i], rest: list};
 	}
 	return list;
-  }
-
+}
 let arr = [1,2,3];
 console.log(arrayToList(arr));
 
-// let list2 = null;
-// list2 = {rest: list2};
-// console.log(list2);
+const listToArray = (list) => {
+	let array = [];
+	for (let node = list; node; node = node.rest) { //if node is true(not null)
+		array.push(node.value);
+	}
+	return array;
+}
+console.log(listToArray(arrayToList(arr)));
+
+const prepend = (newValue, list) => {
+	return {value: newValue, rest: list};
+}
+console.log(prepend(10, prepend(20, null)));
+
+const nth = (list, n) => {
+	if (!list) return undefined; //not true
+	else if (n == 0) return list.value;
+	else return nth(list.rest, n-1);
+}
+console.log(nth(arrayToList([10, 20, 30]), 1));
---
+++ note/exercise_eloquent_JS/10_deep_comparison.js
@@ -0,0 +1,5 @@
+const deepEqual = (a, b) => {
+	if ((typeof(a) == typeof(b)) && a==b) {
+		return true;
+	} else return false;
+}
