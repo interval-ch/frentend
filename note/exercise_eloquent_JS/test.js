c86fd5bc90920f19e0144401c6faa026b5a32991
interval-ch <60610169+interval-ch@users.noreply.github.com>
2022-08-18 16:12:05 +0800

    object

--- note/exercise_eloquent_JS/test.js
+++ note/exercise_eloquent_JS/test.js
@@ -1,8 +1,26 @@
-const person = {
-	name: ['Bob', 'Smith'],
-  };
-
-const one = [];
-
-console.log(typeof person.name);
-console.log(typeof one);
+class Person {
+	name;
+	constructor(name) {
+	  this.name = name;
+	}
+	introduceSelf() {
+	  console.log(`Hi! I'm ${this.name}`);
+	}
+  }
+class Professor extends Person {
+	teaches;
+	constructor(name, teaches) {
+		super(name);
+		this.teaches = teaches;
+	}
+	introduceSelf() {
+		console.log(`My name is ${this.name}, and I will be your ${this.teaches} professor.`);
+	}
+	grade(paper) {
+		const grade = Math.floor(Math.random() * (5 - 1) + 1);
+		console.log(grade);
+	}
+}
+const walsh = new Professor('Walsh', 'Psychology');
+walsh.introduceSelf();
+walsh.grade();
