c628fb6d904d0283c08129b9d98e1513ef69a1b3
interval-ch <60610169+interval-ch@users.noreply.github.com>
2022-09-04 13:59:05 +0800

    what

--- note/mdn/javascript/codeTest.js
+++ note/mdn/javascript/codeTest.js
@@ -1,20 +1,7 @@
-function sumToLoop(n) {
-	let ans = 0;
-	for (let i = 0; i <= n; i++) {
-		ans += i;
-	}
-	return ans;
-}
+let arr = [2,4,3];
+let ans;
+ans = arr.reduceRight(0, current, index, arr => {
+	Math.pow(10,((arr.length()-1)-index));
+}, 0);
 
-function sumToRecursion(n) {
-	if (n == 1) return 1;
-	else return n + sumToRecursion(n-1)
-}
-
-function sumToArithmetic(n) {
-	return (1+n)*n*0.5
-}
-
-console.log(sumToLoop(100000));
-console.log(sumToRecursion(100000));
-console.log(sumToArithmetic(100000));
+console.log(and);
