204d1dc9fa5a53440b8374693bbb7c67852a32ec
interval-ch <60610169+interval-ch@users.noreply.github.com>
2022-09-05 12:54:14 +0800

    not correct

--- note/mdn/javascript/codeTest.js
+++ note/mdn/javascript/codeTest.js
@@ -1,4 +1,15 @@
-var a = 'aba';
-console.log(globalThis.a);
-console.log(global);
+function myFunction(a, b) {
+	let done = 0;
+	let count = 0;
+	while (true) {
+		let found = a.indexOf(b, done);
+		if (found == -1) {break
+		} else { 
+		count += 1;
+		done = found;
+		}
+	}
+	return count;
+}
 
+console.log(myFunction('m', 'how many times does the character occur in this sentence?'));
