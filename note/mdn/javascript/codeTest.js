582af6c5b80d0b052a51b9f9ea7ff11c786468b1
interval_ch <hao12654@gmail.com>
2022-09-16 21:38:20 +0800

    220916

--- note/mdn/javascript/codeTest.js
+++ note/mdn/javascript/codeTest.js
@@ -1,17 +1,2 @@
-function
-myFunction
-(
-a, b
-)
-{
-let arr = [];   
-let c = a;
-for (let i = 0; i < (Math.ceil(a.length/3)); i++) {
-  console.log(i);
-  arr.unshift(c.slice(-3));
-  c = c.slice(0,-3);
-} 
-console.log(arr);
-return arr.join(b);
-}
-console.log(myFunction('1234567','.'));
+let a = new Array(4).fill(5);
+console.log(a);
