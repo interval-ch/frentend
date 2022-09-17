97b4a6f8d8cd2e7f1d1c188d82f0c51405d6013f
interval_ch <hao12654@gmail.com>
2022-09-18 01:23:00 +0800

    hard

--- note/leetCode/335_SelfCrossing.js
+++ note/leetCode/335_SelfCrossing.js
@@ -39,5 +39,76 @@
 //   });
 // 	return cross;
 // };
-//// runtime error
+//// runtime error, too slow;
 
+var isSelfCrossing = function(x) {
+	if (x.length <= 3) {
+					return false;
+			}
+			let i = 2;
+			// keep spiraling outward
+			while (i < x.length && x[i] > x[i - 2]) {
+					i++;
+			}
+			if (i >= x.length) {
+					return false;
+			}
+			// transition from spiraling outward to spiraling inward
+			if ((i >= 4 && x[i] >= x[i - 2] - x[i - 4]) ||
+							(i == 3 && x[i] == x[i - 2])) {
+					x[i - 1] -= x[i - 3];
+			}
+			i++;
+			// keep spiraling inward
+			while (i < x.length) {
+					if (x[i] >= x[i - 2]) {
+							return true;
+					}
+					i++;
+			}
+			return false;
+};
+
+//from web
+
+/**
+ * @param {number[]} distance
+ * @return {boolean}
+ */
+ var isSelfCrossing2 = function(distance) {
+  
+  
+  for (let i = 0; i < distance.length; i++) {
+    let a = distance[i];
+    let b = distance[i-1];
+    let c = distance[i-2];
+    let d = distance[i-3];
+    let e = distance[i-4];
+    let f = distance[i-5];
+    
+  //q-shape  
+    if (d) {
+      if (b<=d && a>=c) {
+        return true;
+      }
+    }
+  
+  //square
+    if (e) {
+      if (b == d && (a+e) >= c) {
+        return true;
+      }
+    }
+    
+  
+  //stair
+    if (f) {
+      if (c>=a && c>=e && d>=b && d>=f && (a+e)>=c && (b+f)>=d) {
+        return true;
+      }
+    }  
+  }
+  return false;
+};
+
+//from web2
