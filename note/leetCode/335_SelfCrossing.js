3a27fbecc09a94420c635d60fd8d95d626d2f663
interval_ch <hao12654@gmail.com>
2022-09-18 00:59:52 +0800

    a

--- note/leetCode/335_SelfCrossing.js
+++ note/leetCode/335_SelfCrossing.js
@@ -2,40 +2,42 @@
  * @param {number[]} distance
  * @return {boolean}
  */
- var isSelfCrossing = function(distance) {
-  const set = new Set();
-	set.add(`0,0`);
-  let x = 0;
-  let y = 0;
-	let cross = false;
+//  var isSelfCrossing = function(distance) {
+//   const set = new Set();
+// 	set.add(`0,0`);
+//   let x = 0;
+//   let y = 0;
+// 	let cross = false;
 
-  //loop through array
-  distance.forEach((value,index) => {
-		//how many step 
-		for (let i = 1; i <= value; i++) {
-			// direction
-			switch ((index+1) % 4) {
-				case 1:
-					y += 1;
-					break;
-				case 2:
-					x -= 1;
-					break;
-				case 3:
-					y -= 1;
-					break;	
-				case 0:
-					x += 1;
-					break;
-			} 
-			//let boo = set.has(`${x},${y}`);
-			//console.log(`i:${i}, x:${x}, y:${y}, set.has:${boo}`)
+//   //loop through array
+//   distance.forEach((value,index) => {
+// 		//how many step 
+// 		for (let i = 1; i <= value; i++) {
+// 			// direction
+// 			switch ((index+1) % 4) {
+// 				case 1:
+// 					y += 1;
+// 					break;
+// 				case 2:
+// 					x -= 1;
+// 					break;
+// 				case 3:
+// 					y -= 1;
+// 					break;	
+// 				case 0:
+// 					x += 1;
+// 					break;
+// 			} 
+// 			//let boo = set.has(`${x},${y}`);
+// 			//console.log(`i:${i}, x:${x}, y:${y}, set.has:${boo}`)
 
-			if (set.has(`${x},${y}`)) {
-				cross =  true;
-			}
-			set.add(`${x},${y}`);
-		}
-  });
-	return cross;
-};
+// 			if (set.has(`${x},${y}`)) {
+// 				cross =  true;
+// 			}
+// 			set.add(`${x},${y}`);
+// 		}
+//   });
+// 	return cross;
+// };
+//// runtime error
+
