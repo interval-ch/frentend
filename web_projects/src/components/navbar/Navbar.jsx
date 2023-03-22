c12466aa51874662b539251f72e92311f88c73a6
interval_ch <hao12654@gmail.com>
2023-03-22 17:55:44 +0800

    for a long delay

--- web_projects/src/components/navbar/Navbar.jsx
+++ web_projects/src/components/navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import './navbar.css';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 
+// BEM => block element modifier
+// maming method?
+
 const Navbar = () => {
 	return (
 		<div className='gpt3__navbar'>
