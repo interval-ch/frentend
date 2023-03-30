88a5dd5c02da014ad00427152b7f9f3c8d1323ec
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-03-30 11:45:51 +0800

    230330

--- web_projects/src/containers/header/Header.jsx
+++ web_projects/src/containers/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 				<p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 				
 				<div className="gpt3__header-content__input">
-					<input type="email" placeholder='Your email' />
+					<input type="email" placeholder='Your Email Address' />
 					<button type='button'>Get Start</button>
 				</div>
 
@@ -19,10 +19,9 @@ const Header = () => {
 					<img src={people} alt="people" />
 					<p>1,600 people requested access a visit in last 24 hours</p>
 				</div>
-
-				{/* <div className="gpt3__header-image">
-					<img src={ai} alt="ai" />
-				</div> */}
+			</div>
+			<div className="gpt3__header-image">
+				<img src={ai} alt="ai" />
 			</div>
 		</div>
 	)
