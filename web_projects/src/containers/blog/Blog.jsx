c6ae8ce2ba04f6e53ecff85acba65d544e18f7d0
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-04-10 09:56:18 +0800

    112

--- web_projects/src/containers/blog/Blog.jsx
+++ web_projects/src/containers/blog/Blog.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import './blog.css'
+import React from 'react';
+import './blog.css';
+
+import { Article } from '../../components';
 
 const Blog = () => {
 	return (
-		<div>Blog</div>
+		<div className="gpt3__blog">
+			<h1>A lot is happening, We are blogging about it.</h1>
+			<div className="gpt3__blog-container">
+				<Article />
+				<Article />
+				<Article />
+				<Article />
+				<Article />
+			</div>
+		</div>
 	)
 }
 
