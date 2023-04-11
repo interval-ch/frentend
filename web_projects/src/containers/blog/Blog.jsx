4f50465b0adbb0af1a37a40419dc070124c37178
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-04-11 10:46:01 +0800

    footer

--- web_projects/src/containers/blog/Blog.jsx
+++ web_projects/src/containers/blog/Blog.jsx
@@ -6,14 +6,14 @@ import { Article } from '../../components';
 const Blog = () => {
 	return (
 		<div className="gpt3__blog section__padding" id='blog'>
-			<div className="gp3__blog-heading">
+			<div className="gpt3__blog-heading">
 				<h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
 			</div>
 			<div className="gpt3__blog-container">
-				<div className="gpt3__blog-container__groupA">
+				<div className="gpt3__blog-container_groupA">
 					<Article imgUrl={blog01} date='Sep 26, 2021' title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
 				</div>
-				<div className="gpt3__blog-container__groupB">
+				<div className="gpt3__blog-container_groupB">
 					<Article imgUrl={blog02} date='Sep 26, 2021' title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
 					<Article imgUrl={blog03} date='Sep 26, 2021' title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
 					<Article imgUrl={blog04} date='Sep 26, 2021' title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
