852a8505583d0a39e9a7ceb6d13b3b6e4752941e
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-04-11 13:01:40 +0800

    footer trial

--- web_projects/src/containers/footer/Footer.jsx
+++ web_projects/src/containers/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 		<div className="gpt3__footer section__padding">
 			<div className="gpt3__footer-heading">
 				<h1 className="gradient__text">
-					h1h1h1
+					Do you want to step in to the future before others
 				</h1>
 				<div className="gpt3__footer-btn">
 					<p>Request Early Access</p>
@@ -16,7 +16,7 @@ const Footer = () => {
 			<div className="gpt3__footer-links">
 				<div className="gpt3__footer-links_logo">
 					<img src={gpt3Logo} alt="logo" />
-					<p>pappapaapapa</p>
+					<p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
 				</div>
 				<div className="gpt3__footer-links-div">
 					<h4>Links</h4>
@@ -37,6 +37,9 @@ const Footer = () => {
 					<p>085-132567</p>
 					<p>info@payme.net</p>
 				</div>
+			</div>
+			<div className="gpt3__footer-copyright">
+				<p>© 2021 GPT-3. All rights reserved.</p>
 			</div>
 		</div>
 	)
