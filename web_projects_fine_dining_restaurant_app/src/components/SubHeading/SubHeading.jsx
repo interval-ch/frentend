18bd2300a75eb879d9250bc3b5ca84df6e70065a
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-04-13 20:06:27 +0800

    start

---
+++ web_projects_fine_dining_restaurant_app/src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './subheading.css'
+
+const SubHeading = () => {
+	return (
+		<div>SubHeading</div>
+	)
+}
+
+export default SubHeading
---
+++ web_projects_fine_dining_restaurant_app/src/container/Manu/Manu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './menu.css'
+
+const Manu = () => {
+	return (
+		<div>Manu</div>
+	)
+}
+
+export default Manu
