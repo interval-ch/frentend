3689e54c9f663006675809adfc84c2e71f20ea05
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-04-19 08:26:11 +0800

    start

--- web_projects_fine_dining_restaurant_app/src/components/Navbar/Navbar.jsx
+++ web_projects_fine_dining_restaurant_app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={ images.gericht } alt="logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#About">About</a></li>
+      <li className="p__opensans"><a href="#Menu">Menu</a></li>
+      <li className="p__opensans"><a href="#Award">Award</a></li>
+      <li className="p__opensans"><a href="#Contact">Contact</a></li>
+    </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Log In / Register</a>
+      <div />
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
+  </nav>
+  );
+};
 
 export default Navbar;
