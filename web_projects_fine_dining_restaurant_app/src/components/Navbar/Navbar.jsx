3ee444fe90a94a7b0ab0dbc4770d73a63ef0508a
interval-ch <60610169+interval-ch@users.noreply.github.com>
2023-04-20 13:29:30 +0800

    navbar

--- web_projects_fine_dining_restaurant_app/src/components/Navbar/Navbar.jsx
+++ web_projects_fine_dining_restaurant_app/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import images from '../../constants/images';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdRestaurantMenu } from "react-icons/md";
 
 import './Navbar.css';
 
@@ -22,6 +24,24 @@ const Navbar = () => {
       <a href="#login" className="p__opensans">Log In / Register</a>
       <div />
       <a href="/" className="p__opensans">Book Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {setToggleMenu(true)}}/>
+      
+      (toggleMenu && 
+
+      <div className="app__navbar-smallscreen_overlay">
+        <MdRestaurantMenu className='overlay__close' color='#fff' fontSize={27} onClick={() => {setToggleMenu(false)}}/>
+        <ul className="app__navbar-links">
+          <li className="p__opensans"><a href="#home">Home</a></li>
+          <li className="p__opensans"><a href="#About">About</a></li>
+          <li className="p__opensans"><a href="#Menu">Menu</a></li>
+          <li className="p__opensans"><a href="#Award">Award</a></li>
+          <li className="p__opensans"><a href="#Contact">Contact</a></li>
+        </ul>
+      </div>
+      )
+      
     </div>
   </nav>
   );
